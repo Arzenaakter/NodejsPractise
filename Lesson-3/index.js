506256e151ce57899fe14181785787fc340edd63
Arzena Akter <arzenaakter@gmail.com>
2023-10-07 12:11:43 +0600

    os, path

---
+++ Lesson-3/index.js
@@ -0,0 +1,18 @@
+const os = require('os');
+
+// console.log(os.userInfo())
+// console.log(os.homedir())
+// console.log(os.hostname())
+// console.log(os.totalmem())
+// console.log(os.freemem())
+// console.log(__dirname);
+// console.log(__filename);
+
+
+const path = require('path');
+
+// const extenName = path.extname('index.html')
+// console.log(extenName)
+
+const joinName = path.join(__dirname, '/../views/')
+console.log(joinName)
