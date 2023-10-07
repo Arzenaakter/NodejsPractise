8eef6fa8fc352f98c92420399f9171c459a4d840
Arzena Akter <arzenaakter@gmail.com>
2023-10-07 20:13:21 +0600

    external module use

---
+++ Lesson-6/index.js
@@ -0,0 +1,6 @@
+const getRandomFruitsName = require('random-fruits-name')
+console.log(getRandomFruitsName('en'))
+
+var moviesNames = require('movies-names');
+// console.log(moviesNames.all)
+console.log(moviesNames.random() )
